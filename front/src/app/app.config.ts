04ee55bb9d7c6d53f64ad323d357972ad35fb757
Jose Antonio Vallecillo Barroso <jose.vallecillo@col.vueling.com>
2026-02-05 19:55:08 +0100

    Add withEnabledBlockingInitialNavigation to router to fix NG0908 error

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -7,7 +7,7 @@ import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(
       withInterceptors([authInterceptor])
     )
